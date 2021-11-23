91840a068ea72fc9f1499bc164b4269ec6918fe3
kauanabrahao <kauan.martins@fatec.sp.gov.br>
2021-11-22 21:05:09 -0300

    meu primeiro commit

---
+++ src/main/java/labdb/labdb/LabdbApplication.java
@@ -0,0 +1,13 @@
+package labdb.labdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabdbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabdbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/labdb/labdb/LabdbApplicationTests.java
@@ -0,0 +1,13 @@
+package labdb.labdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabdbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
